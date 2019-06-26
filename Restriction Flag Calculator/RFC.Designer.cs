b27f329edb60974d502705bebfc27e43e21c10e6
Skawo <skawo90@gmail.com>
2019-06-26 17:48:20 +0200

    Initial commit

---
+++ Restriction Flag Calculator/RFC.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Restriction_Flag_Calculator
+{
+    partial class RFC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Bottles = new System.Windows.Forms.CheckBox();
+            this.AButton = new System.Windows.Forms.CheckBox();
+            this.BButton = new System.Windows.Forms.CheckBox();
+            this.Health = new System.Windows.Forms.CheckBox();
+            this.WarpSongs = new System.Windows.Forms.CheckBox();
+            this.Ocarina = new System.Windows.Forms.CheckBox();
+            this.Hookshot = new System.Windows.Forms.CheckBox();
+            this.TradingItems = new System.Windows.Forms.CheckBox();
+            this.Global = new System.Windows.Forms.CheckBox();
+            this.DinPNayruL = new System.Windows.Forms.CheckBox();
+            this.FaroreWind = new System.Windows.Forms.CheckBox();
+            this.SunSong = new System.Windows.Forms.CheckBox();
+            this.Label_Title = new System.Windows.Forms.Label();
+            this.Label_Result = new System.Windows.Forms.Label();
+            this.NumUpDown_SceneNumber = new System.Windows.Forms.NumericUpDown();
+            this.Label_SceneNumber = new System.Windows.Forms.Label();
+            this.NumUpDown_Result = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.NumUpDown_SceneNumber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumUpDown_Result)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Bottles
+            // 
+            this.Bottles.AutoSize = true;
+            this.Bottles.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bottles.Location = new System.Drawing.Point(16, 240);
+            this.Bottles.Name = "Bottles";
+            this.Bottles.Size = new System.Drawing.Size(68, 20);
+            this.Bottles.TabIndex = 0;
+            this.Bottles.Text = "Bottles";
+            this.Bottles.UseVisualStyleBackColor = true;
+            this.Bottles.CheckedChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // AButton
+            // 
+            this.AButton.AutoSize = true;
+            this.AButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AButton.Location = new System.Drawing.Point(16, 266);
+            this.AButton.Name = "AButton";
+            this.AButton.Size = new System.Drawing.Size(76, 20);
+            this.AButton.TabIndex = 1;
+            this.AButton.Text = "A Button";
+            this.AButton.UseVisualStyleBackColor = true;
+            this.AButton.CheckedChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // BButton
+            // 
+            this.BButton.AutoSize = true;
+            this.BButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BButton.Location = new System.Drawing.Point(16, 292);
+            this.BButton.Name = "BButton";
+            this.BButton.Size = new System.Drawing.Size(76, 20);
+            this.BButton.TabIndex = 2;
+            this.BButton.Text = "B Button";
+            this.BButton.UseVisualStyleBackColor = true;
+            this.BButton.CheckedChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // Health
+            // 
+            this.Health.AutoSize = true;
+            this.Health.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Health.Location = new System.Drawing.Point(16, 318);
+            this.Health.Name = "Health";
+            this.Health.Size = new System.Drawing.Size(110, 20);
+            this.Health.TabIndex = 3;
+            this.Health.Text = "Health Gauge";
+            this.Health.UseVisualStyleBackColor = true;
+            this.Health.CheckedChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // WarpSongs
+            // 
+            this.WarpSongs.AutoSize = true;
+            this.WarpSongs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WarpSongs.Location = new System.Drawing.Point(16, 136);
+            this.WarpSongs.Name = "WarpSongs";
+            this.WarpSongs.Size = new System.Drawing.Size(102, 20);
+            this.WarpSongs.TabIndex = 4;
+            this.WarpSongs.Text = "Warp Songs";
+            this.WarpSongs.UseVisualStyleBackColor = true;
+            this.WarpSongs.CheckedChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // Ocarina
+            // 
+            this.Ocarina.AutoSize = true;
+            this.Ocarina.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ocarina.Location = new System.Drawing.Point(16, 162);
+            this.Ocarina.Name = "Ocarina";
+            this.Ocarina.Size = new System.Drawing.Size(74, 20);
+            this.Ocarina.TabIndex = 5;
+            this.Ocarina.Text = "Ocarina";
+            this.Ocarina.UseVisualStyleBackColor = true;
+            this.Ocarina.CheckedChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // Hookshot
+            // 
+            this.Hookshot.AutoSize = true;
+            this.Hookshot.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hookshot.Location = new System.Drawing.Point(16, 188);
+            this.Hookshot.Name = "Hookshot";
+            this.Hookshot.Size = new System.Drawing.Size(85, 20);
+            this.Hookshot.TabIndex = 6;
+            this.Hookshot.Text = "Hookshot";
+            this.Hookshot.UseVisualStyleBackColor = true;
+            this.Hookshot.CheckedChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // TradingItems
+            // 
+            this.TradingItems.AutoSize = true;
+            this.TradingItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TradingItems.Location = new System.Drawing.Point(16, 214);
+            this.TradingItems.Name = "TradingItems";
+            this.TradingItems.Size = new System.Drawing.Size(147, 20);
+            this.TradingItems.TabIndex = 7;
+            this.TradingItems.Text = "Trading Quest Items";
+            this.TradingItems.UseVisualStyleBackColor = true;
+            this.TradingItems.CheckedChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // Global
+            // 
+            this.Global.AutoSize = true;
+            this.Global.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Global.Location = new System.Drawing.Point(16, 32);
+            this.Global.Name = "Global";
+            this.Global.Size = new System.Drawing.Size(67, 20);
+            this.Global.TabIndex = 8;
+            this.Global.Text = "Global";
+            this.Global.UseVisualStyleBackColor = true;
+            this.Global.CheckedChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // DinPNayruL
+            // 
+            this.DinPNayruL.AutoSize = true;
+            this.DinPNayruL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DinPNayruL.Location = new System.Drawing.Point(16, 58);
+            this.DinPNayruL.Name = "DinPNayruL";
+            this.DinPNayruL.Size = new System.Drawing.Size(166, 20);
+            this.DinPNayruL.TabIndex = 9;
+            this.DinPNayruL.Text = "Din\'s Fire/Nayru\'s Love";
+            this.DinPNayruL.UseVisualStyleBackColor = true;
+            this.DinPNayruL.CheckedChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // FaroreWind
+            // 
+            this.FaroreWind.AutoSize = true;
+            this.FaroreWind.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FaroreWind.Location = new System.Drawing.Point(16, 84);
+            this.FaroreWind.Name = "FaroreWind";
+            this.FaroreWind.Size = new System.Drawing.Size(111, 20);
+            this.FaroreWind.TabIndex = 10;
+            this.FaroreWind.Text = "Farore\'s Wind";
+            this.FaroreWind.UseVisualStyleBackColor = true;
+            this.FaroreWind.CheckedChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // SunSong
+            // 
+            this.SunSong.AutoSize = true;
+            this.SunSong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SunSong.Location = new System.Drawing.Point(16, 110);
+            this.SunSong.Name = "SunSong";
+            this.SunSong.Size = new System.Drawing.Size(95, 20);
+            this.SunSong.TabIndex = 11;
+            this.SunSong.Text = "Sun\'s Song";
+            this.SunSong.UseVisualStyleBackColor = true;
+            this.SunSong.CheckedChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // Label_Title
+            // 
+            this.Label_Title.AutoSize = true;
+            this.Label_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Title.Location = new System.Drawing.Point(12, 9);
+            this.Label_Title.Name = "Label_Title";
+            this.Label_Title.Size = new System.Drawing.Size(161, 20);
+            this.Label_Title.TabIndex = 12;
+            this.Label_Title.Text = "Restrict the usage of:";
+            // 
+            // Label_Result
+            // 
+            this.Label_Result.AutoSize = true;
+            this.Label_Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Result.Location = new System.Drawing.Point(12, 396);
+            this.Label_Result.Name = "Label_Result";
+            this.Label_Result.Size = new System.Drawing.Size(59, 20);
+            this.Label_Result.TabIndex = 14;
+            this.Label_Result.Text = "Result:";
+            // 
+            // NumUpDown_SceneNumber
+            // 
+            this.NumUpDown_SceneNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.NumUpDown_SceneNumber.Location = new System.Drawing.Point(16, 364);
+            this.NumUpDown_SceneNumber.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.NumUpDown_SceneNumber.Name = "NumUpDown_SceneNumber";
+            this.NumUpDown_SceneNumber.Size = new System.Drawing.Size(90, 26);
+            this.NumUpDown_SceneNumber.TabIndex = 15;
+            this.NumUpDown_SceneNumber.ValueChanged += new System.EventHandler(this.EditorChanged);
+            // 
+            // Label_SceneNumber
+            // 
+            this.Label_SceneNumber.AutoSize = true;
+            this.Label_SceneNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_SceneNumber.Location = new System.Drawing.Point(12, 341);
+            this.Label_SceneNumber.Name = "Label_SceneNumber";
+            this.Label_SceneNumber.Size = new System.Drawing.Size(74, 20);
+            this.Label_SceneNumber.TabIndex = 16;
+            this.Label_SceneNumber.Text = "In scene:";
+            // 
+            // NumUpDown_Result
+            // 
+            this.NumUpDown_Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.NumUpDown_Result.Location = new System.Drawing.Point(16, 420);
+            this.NumUpDown_Result.Maximum = new decimal(new int[] {
+            -1,
+            0,
+            0,
+            0});
+            this.NumUpDown_Result.Name = "NumUpDown_Result";
+            this.NumUpDown_Result.Size = new System.Drawing.Size(194, 26);
+            this.NumUpDown_Result.TabIndex = 17;
+            this.NumUpDown_Result.ValueChanged += new System.EventHandler(this.ResultChanged);
+            // 
+            // RFC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 458);
+            this.Controls.Add(this.NumUpDown_Result);
+            this.Controls.Add(this.Label_SceneNumber);
+            this.Controls.Add(this.NumUpDown_SceneNumber);
+            this.Controls.Add(this.Label_Result);
+            this.Controls.Add(this.Label_Title);
+            this.Controls.Add(this.SunSong);
+            this.Controls.Add(this.FaroreWind);
+            this.Controls.Add(this.DinPNayruL);
+            this.Controls.Add(this.Global);
+            this.Controls.Add(this.TradingItems);
+            this.Controls.Add(this.Hookshot);
+            this.Controls.Add(this.Ocarina);
+            this.Controls.Add(this.WarpSongs);
+            this.Controls.Add(this.Health);
+            this.Controls.Add(this.BButton);
+            this.Controls.Add(this.AButton);
+            this.Controls.Add(this.Bottles);
+            this.Name = "RFC";
+            this.Text = "RFC";
+            ((System.ComponentModel.ISupportInitialize)(this.NumUpDown_SceneNumber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumUpDown_Result)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox Bottles;
+        private System.Windows.Forms.CheckBox AButton;
+        private System.Windows.Forms.CheckBox BButton;
+        private System.Windows.Forms.CheckBox Health;
+        private System.Windows.Forms.CheckBox WarpSongs;
+        private System.Windows.Forms.CheckBox Ocarina;
+        private System.Windows.Forms.CheckBox Hookshot;
+        private System.Windows.Forms.CheckBox TradingItems;
+        private System.Windows.Forms.CheckBox Global;
+        private System.Windows.Forms.CheckBox DinPNayruL;
+        private System.Windows.Forms.CheckBox FaroreWind;
+        private System.Windows.Forms.CheckBox SunSong;
+        private System.Windows.Forms.Label Label_Title;
+        private System.Windows.Forms.Label Label_Result;
+        private System.Windows.Forms.NumericUpDown NumUpDown_SceneNumber;
+        private System.Windows.Forms.Label Label_SceneNumber;
+        private System.Windows.Forms.NumericUpDown NumUpDown_Result;
+    }
+}
+
---
+++ Restriction Flag Calculator/RFC.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restriction_Flag_Calculator
+{
+    public partial class RFC : Form
+    {
+        public RFC()
+        {
+            InitializeComponent();
+        }
+
+        private void EditorChanged(object sender, EventArgs e)
+        {
+            NumUpDown_Result.ValueChanged -= ResultChanged;
+
+            byte FirstByte = (byte)((Global.Checked ? 1 : 0) + (DinPNayruL.Checked ? 4 : 0) + (FaroreWind.Checked ? 16 : 0) + (SunSong.Checked ? 64 : 0));
+            byte SecondByte = (byte)((WarpSongs.Checked ? 1 : 0) + (Ocarina.Checked ? 4 : 0) + (Hookshot.Checked ? 16 : 0) + (TradingItems.Checked ? 64 : 0));
+            byte ThirdByte = (byte)((Bottles.Checked ? 1 : 0) + (AButton.Checked ? 4 : 0) + (BButton.Checked ? 16 : 0) + (Health.Checked ? 64 : 0));
+
+            byte[] Result = new byte[4];
+
+            Result[0] = FirstByte;
+            Result[1] = SecondByte;
+            Result[2] = ThirdByte;
+            Result[3] = Convert.ToByte(NumUpDown_SceneNumber.Value);
+
+            NumUpDown_Result.Text = BitConverter.ToUInt32(Result, 0).ToString();
+
+            NumUpDown_Result.ValueChanged += ResultChanged;
+        }
+
+        private void ResultChanged(object sender, EventArgs e)
+        {
+            foreach (Control C in this.Controls)
+                if (C is CheckBox)
+                    (C as CheckBox).CheckedChanged -= EditorChanged;
+
+            NumUpDown_SceneNumber.ValueChanged -= EditorChanged;
+
+            uint Result = (uint)(NumUpDown_Result.Value);
+
+            byte[] Bytes = BitConverter.GetBytes(Result);
+
+            byte SceneID = Bytes[3];
+            byte ThirdByte = Bytes[2];
+            byte SecondByte = Bytes[1];
+            byte FirstByte = Bytes[0];
+
+            NumUpDown_SceneNumber.Value = Convert.ToInt32(SceneID);
+
+            Global.Checked = Convert.ToBoolean(FirstByte & 1) | Convert.ToBoolean(FirstByte & 2);
+            DinPNayruL.Checked = Convert.ToBoolean(FirstByte & 4) | Convert.ToBoolean(FirstByte & 8);
+            FaroreWind.Checked = Convert.ToBoolean(FirstByte & 16) | Convert.ToBoolean(FirstByte & 32);
+            SunSong.Checked = Convert.ToBoolean(FirstByte & 64) | Convert.ToBoolean(FirstByte & 128);
+
+            WarpSongs.Checked = Convert.ToBoolean(SecondByte & 1) | Convert.ToBoolean(FirstByte & 2);
+            Ocarina.Checked = Convert.ToBoolean(SecondByte & 4) | Convert.ToBoolean(FirstByte & 8);
+            Hookshot.Checked = Convert.ToBoolean(SecondByte & 16) | Convert.ToBoolean(FirstByte & 32);
+            TradingItems.Checked = Convert.ToBoolean(SecondByte & 64) | Convert.ToBoolean(FirstByte & 128);
+
+            Bottles.Checked = Convert.ToBoolean(ThirdByte & 1) | Convert.ToBoolean(FirstByte & 2);
+            AButton.Checked = Convert.ToBoolean(ThirdByte & 4) | Convert.ToBoolean(FirstByte & 8);
+            BButton.Checked = Convert.ToBoolean(ThirdByte & 16) | Convert.ToBoolean(FirstByte & 32);
+            Health.Checked = Convert.ToBoolean(ThirdByte & 64) | Convert.ToBoolean(FirstByte & 128);
+
+            foreach (Control C in this.Controls)
+                if (C is CheckBox)
+                    (C as CheckBox).CheckedChanged += EditorChanged;
+
+            NumUpDown_SceneNumber.ValueChanged += EditorChanged;
+        }
+    }
+}
